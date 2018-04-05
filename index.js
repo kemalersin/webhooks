4e7ba52697575a108b44f1799f4105320773398b
Kemal Ersin YILMAZ <mail@kemalersin.com>
2018-04-05 13:42:39 +0300

    Git force pull to overwrite local files.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 require('dotenv').config()
 
+const ips = [
+  '127.0.0.1',
+  '192.30.252.0/22',
+  '185.199.108.0/22'
+]
+
 const express = require('express')
 const mongoose = require('mongoose')
 const helmet = require('helmet')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const bluebird = require('bluebird')
+
+const ipfilter = require('express-ipfilter').IpFilter
 
 const config = require('./config')
 const routes = require('./routes')
@@ -16,6 +24,7 @@ mongoose.Promise = bluebird
 mongoose.connect(config.mongo.url)
 
 app.use(helmet())
+app.use(ipfilter(ips, {mode: 'allow'}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
