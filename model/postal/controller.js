627617a3795f1f7581cd7b74688eb163086f31b3
Kemal Ersin YILMAZ <mail@kemalersin.com>
2018-04-20 12:05:07 +0300

    Fix Zogo image issue...

--- model/postal/controller.js
+++ model/postal/controller.js
@@ -11,12 +11,29 @@ PostalController.prototype.test = function (req, res, next) {
   let message = new Postal.SendMessage(client)
 
   message.from('postal@kemalersin.com')
+  
   message.to('mail@kemalersin.com')
+  message.to('kemalersinyilmaz@gmail.com')
+  message.to('kemal@olimpus.com.tr')
+  
+  message.cc('kemalersin@outlook.com')
+  message.bcc('kemalersin@icloud.com')
 
   message.subject('Postal Test')
+
   message.htmlBody(
+    '<!DOCTYPE html>' +
+    '<html xmlns="http://www.w3.org/1999/xhtml">' +
+    '<head>' +
+    '<title>Postal Test</title>' +
+    '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />' +
+    '</head>' +
+    '<body>' +
     '<p>Postal Test</p>' +
-    '<p><a href="https://www.google.com">Google</a></p>'
+    '<p><a href="https://www.google.com">Google</a></p>' +
+    '<p><img src="http://via.placeholder.com/350x150" width="350" height="150" /></p>' +
+    '</body>' +
+    '</html>' 
   )
 
   message.send().
